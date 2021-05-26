bb16a045df9b159fb79ecc365333ce62d5545b17
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-26 19:25:41 +0100

    schema

---
+++ src/schema/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8 , 2 ),
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id)
+        REFERENCES department (id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id)
+        REFERENCES role (id) ON DELETE CASCADE,
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id)
+        REFERENCES employee (id) ON DELETE SET NULL
+);
